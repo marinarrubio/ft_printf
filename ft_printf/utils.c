e79ff2c58f337cc4f8c66c8d23cc947414752644
marina_rubio <122643043+marinarrubio@users.noreply.github.com>
2023-03-27 11:04:22 +0200

    Add files via upload

---
+++ ft_printf/utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marubio- <marubio-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 10:18:35 by marubio-          #+#    #+#             */
+/*   Updated: 2023/03/22 12:24:37 by marubio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (s && *s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	n;
+
+	n = 0;
+	while (str[n])
+		n++;
+	return (n);
+}
